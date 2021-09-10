b7f0c0fa8b3aed522aa03e371433493f83cedb16
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-11 00:08:42 +0200

    Initial Commit

---
+++ Shared/CustomHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHeaderApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
